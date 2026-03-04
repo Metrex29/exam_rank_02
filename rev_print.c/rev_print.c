db54ec0d1845adba3c1d582d81879401b31dc33b
Metrex29 <carlospicon80@gmail.com>
2026-03-04 16:12:04 +0100

    prueba

---
+++ rev_print.c/rev_print.c
@@ -0,0 +1,47 @@
+/* Assignment name  : rev_print
+Expected files   : rev_print.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string, and displays the string in reverse
+followed by a newline.
+
+If the number of parameters is not 1, the program displays a newline.
+
+Examples:
+
+$> ./rev_print "zaz" | cat -e
+zaz$
+$> ./rev_print "dub0 a POIL" | cat -e
+LIOP a 0bud$
+$> ./rev_print | cat -e
+$ */
+
+int strlen_ft(char *s)
+{
+    int i = 0;
+    while(s[i])
+        i++;
+    return i;
+}
+
+
+int main(int ac, char **av)
+{
+    char *arr;
+    int i = 0;
+    if(ac == 2)
+    {
+        int len = strlen_ft(av[1]);
+        
+        while(len != 0)
+        {
+            arr[len] = av[1][len--];
+        }
+        while(arr[i])
+        {
+            write(1, &arr[i] ,1);
+        }
+    }
+    
+}
